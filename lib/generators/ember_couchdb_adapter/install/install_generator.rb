c82dbb2d690156ddeccd5e8d4998de520d17420e
Opak Alex <opak.alexandr@gmail.com>
2013-07-02 22:04:34 +0300

    update path to copy js

--- lib/generators/ember_couchdb_adapter/install/install_generator.rb
+++ lib/generators/ember_couchdb_adapter/install/install_generator.rb
@@ -6,7 +6,7 @@ module EmberCouchdbAdapter
       source_root File.expand_path('../../../../../assets/javascripts', __FILE__)
 
       def copy
-        copy_file "ember-couchdb-adapter.coffee", "vendor/javascripts/ember-couchdb-adapter.coffee"
+        copy_file "ember-couchdb-adapter.coffee", "vendor/assets/javascripts/ember-couchdb-adapter.coffee"
       end
 
     end
