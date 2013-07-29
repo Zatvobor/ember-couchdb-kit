4de64bfcb2dc01ef6111a217154b0b39cefe9c61
Zatvor <a.zatvornitskiy@gmail.com>
2013-07-29 16:02:14 +0300

    Added renamed package names

--- lib/generators/ember_couchdb_adapter/install/install_generator.rb
+++ lib/generators/ember_couchdb_adapter/install/install_generator.rb
@@ -2,14 +2,16 @@ module EmberCouchdbAdapter
   module Generators
     class InstallGenerator < ::Rails::Generators::Base
 
-      desc "This generator installs ember adapter for couchdb"
+      desc "This generator installs ember-couchdb-kit into vendor/assets/javascripts"
       source_root File.expand_path('../../../../../src', __FILE__)
 
       def copy
-        %w(couchdb-adapter.coffee couchdb-attachment-adapter.coffee couchdb-revs-adapter.coffee
-          ember-couchdb-kit.coffee).each do |file|
-          remove_file "vendor/assets/javascripts/#{file}"
-          copy_file "#{file}", "vendor/assets/javascripts/#{file}"
+        %w(document-adapter attachment-adapter revs-adapter changes-feed ember-couchdb-kit).each do |package|
+          filename      = "#{package}.coffee"
+          vendored_file = "vendor/assets/javascripts/#{filename}"
+
+          remove_file(vendored_file)
+          copy_file(filename, vendored_file)
         end
       end
     end
