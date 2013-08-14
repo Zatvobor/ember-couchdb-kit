2d7dc424ac26d28718d31b1922808e77d245802a
Zatvor <a.zatvornitskiy@gmail.com>
2013-08-14 13:59:44 +0300

    Improved code structure. Added load issue action into particular board

--- example_app/js/app.js
+++ example_app/js/app.js
@@ -15,14 +15,19 @@ App.Store.registerAdapter('App.Issue', EmberCouchDBKit.DocumentAdapter.extend({d
 
 App.IndexRoute = Ember.Route.extend({
   setupController: function(controller, model) {
-    issues = App.Issue.find({type: "view", designDoc: 'issues', viewName: "all", options: 'include_docs=true'})
-    this.get('controller').set('content', issues);
+    self = this;
+    ['common', 'intermediate', 'advanced'].forEach(function(type) {
+       options = 'key="%@"&include_docs=true'.fmt(type);
+       issues = App.Issue.find({type: "view", designDoc: 'issues', viewName: "all_by_board", options: options});
+       self.controllerFor(type).set('content', issues);
+    });
   },
   renderTemplate: function() {
     this.render();
-    this.render('basic',{outlet: 'basic', into: 'index', controller: 'Basic'});
-    this.render('intermediate',{outlet: 'intermediate', into: 'index', controller: 'Intermediate'});
-    this.render('advanced',{outlet: 'advanced', into: 'index', controller: 'Advanced'});
+
+    this.render('board',{outlet: 'common', into: 'index', controller: 'common'});
+    this.render('board',{outlet: 'intermediate', into: 'index', controller: 'intermediate'});
+    this.render('board',{outlet: 'advanced', into: 'index', controller: 'advanced'});
   }
 });
 
@@ -33,17 +38,18 @@ App.IndexController = Ember.ArrayController.extend({
   }
 });
 
-App.BasicController = App.IndexController.extend( {
-  name: 'Basic'
+App.CommonController = App.IndexController.extend( {
+  name: 'common'
 } );
 
 App.IntermediateController = App.IndexController.extend( {
-  name: 'Intermediate'
+  name: 'intermediate'
 } );
 
 App.AdvancedController = App.IndexController.extend( {
-  name: 'Advanced'
+  name: 'advanced'
 } );
+
 
 App.NewIssueView = Ember.View.extend({
   tagName: "form",
@@ -51,7 +57,7 @@ App.NewIssueView = Ember.View.extend({
   submit: function(event){
     event.preventDefault();
     if (this.get('create')){
-      this.get('controller').send("createIssue", {text: this.get("TextArea.value"), board: this.get('board')} );
+      this.get('controller').send("createIssue", {text: this.get("TextArea.value"), board: this.get('controller.name')} );
     }
     this.toggleProperty('create');
   }
